ea2390341aed4bdf583ff504c7e9ea3868aac596
jorgenbs <jorgenbs@gmail.com>
2013-08-30 12:56:31 +0200

    now renders text

--- public/js/message-feed.js
+++ public/js/message-feed.js
@@ -7,11 +7,25 @@
  */
 require(['socket.io', 'jquery'], function(io,$) {
     var socket = io.connect('http://localhost');
-    console.log(socket);
+    var width = $(document).width();
+    var height = $(document).height();
+
     socket.on('newMessage', function (msg) {
-        console.log('GOT MESSAGE');
-        console.log(msg);
-        $('#inbox').text(msg.message);
+        var element = $("<p class='messageText'>" + msg.message + "</p>");
+        randomPosition(element);
+
+        $('#inbox').append(element);
     });
+
+    var randomPosition = function (element) {
+        var offset_x = element.text().length * 7; //ca
+        var offset_y = 100;
+        var rnd_x = Math.floor(Math.random() * (width-offset_x));
+        var rnd_y = Math.floor(Math.random() * (height-offset_y));
+
+        element.css('left',rnd_x);
+        element.css('top',rnd_y);
+    }
+
 });
 
